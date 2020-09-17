361da014f708666bbd21920b9b4df310d5727c15
tainaoual <Tai Naoual>
2020-09-17 16:13:56 -0400

    corection upperlowercase

--- src/main/java/org/wcci/StringProblems.java
+++ src/main/java/org/wcci/StringProblems.java
@@ -56,57 +56,57 @@ public class StringProblems {
     public String evenUpperCaseOrOddLowerCase(String str) {
 
         int a = str.length() % 2;
+
         System.out.println("a is : " + a);
         if (a == 0) {
-            str.toUpperCase();
-        } else {
-            str.toLowerCase();
+            return str.toUpperCase();
         }
-        return str;
-    }
-    /*
-     * stringCombiner()
-     *
-     * Given two strings, stringA and stringB, return a new string that combines the two strings, but the strings should
-     * be combined in ascending alphabetically order.
-     *
-     * Examples:
-     * - stringCombiner("Hello", "There") -> "HelloThere"
-     * - stringCombiner("Zizzer" "zazzer") -> "zazzerZizzer"
-     */
+            return str.toLowerCase();
 
-    public String stringCombiner(String stringA, String stringB) {
-        String c = "";
-        char[] charA = stringA.toCharArray();
-        char[] charB = stringB.toCharArray();
-        for (int i = 0; i < charA.length; i++) {
-            for (int j = 0; j < charB.length; j++) {
-                if ((int) (charA[i]) > (int) (charB[j])) {
-                    c = stringA + stringB;
-                } else {
-                    c = stringB + stringA;
+        }
+        /*
+         * stringCombiner()
+         *
+         * Given two strings, stringA and stringB, return a new string that combines the two strings, but the strings should
+         * be combined in ascending alphabetically order.
+         *
+         * Examples:
+         * - stringCombiner("Hello", "There") -> "HelloThere"
+         * - stringCombiner("Zizzer" "zazzer") -> "zazzerZizzer"
+         */
 
+        public String stringCombiner (String stringA, String stringB){
+            String c = "";
+            char[] charA = stringA.toCharArray();
+            char[] charB = stringB.toCharArray();
+            for (int i = 0; i < charA.length; i++) {
+                for (int j = 0; j < charB.length; j++) {
+                    if ((int) (charA[i]) > (int) (charB[j])) {
+                        c = stringA + stringB;
+                    } else {
+                        c = stringB + stringA;
+
+                    }
                 }
             }
+            return c;
+
         }
-        return c;
+        /*
+         * left2()
+         * Given a string, str, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length
+         * will be at least 2.
+         *
+         * Examples:
+         * - left2("Hello") → "lloHe"
+         * - left2("java") → "vaja"
+         * - left2("Hi") → "Hi"
+
+         */
+
+
+        public String left2 (String str){
+            return str.substring(2) + str.substring(0, 2);
+        }
 
     }
-    /*
-     * left2()
-     * Given a string, str, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length
-     * will be at least 2.
-     *
-     * Examples:
-     * - left2("Hello") → "lloHe"
-     * - left2("java") → "vaja"
-     * - left2("Hi") → "Hi"
-
-     */
-
-
-    public String left2(String str) {
-        return str.substring(2) + str.substring(0, 2);
-    }
-
-}
